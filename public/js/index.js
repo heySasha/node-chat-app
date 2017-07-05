d0514e6adbec7d8836586ed580e7eb33eca48b6f
heySasha <iatsenko.o@donnu.edu.ua>
2017-07-05 18:08:44 +0300

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+socket.on('connect', () => {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        from: 'Andrew',
+        text: 'Yup'
+    });
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', message => {
+   console.log('new Email', message);
+});
